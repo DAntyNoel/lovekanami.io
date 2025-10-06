14b59c266f3381c9af134d90472129f364678ac7
DAnty-mac <dantynoel@sjtu.edu.cn>
2025-10-06 13:54:03 +0800

    rebuild structure

--- script.js
+++ script.js
@@ -1,23 +1,20 @@
 const images = [
-    "images/kanami1.jpg",
-    "images/kanami2.jpg",
-    "images/kanami3.jpg",
-    "images/kanami4.jpg"
-  ];
+    "res/images/backgrounds/Be-Shinning.png",
+    "res/images/backgrounds/Soda.png",
+];
   
-  let index = 0;
-  const bg = document.querySelector('.background');
-  
-  function changeBackground() {
-    bg.style.opacity = 0;
-    setTimeout(() => {
-      bg.style.backgroundImage = `url(${images[index]})`;
-      bg.style.opacity = 1;
-      index = (index + 1) % images.length;
-    }, 1000);
-  }
-  
-  // 初始设置
-  bg.style.backgroundImage = `url(${images[0]})`;
-  setInterval(changeBackground, 5000); // 每 5 秒切换一次
-  
+let index = 0;
+const bg = document.querySelector('.background');
+
+function changeBackground() {
+bg.style.opacity = 0;
+setTimeout(() => {
+    bg.style.backgroundImage = `url(${images[index]})`;
+    bg.style.opacity = 1;
+    index = (index + 1) % images.length;
+}, 1000);
+}
+
+// 初始设置
+bg.style.backgroundImage = `url(${images[0]})`;
+setInterval(changeBackground, 5000); // 每 5 秒切换一次
